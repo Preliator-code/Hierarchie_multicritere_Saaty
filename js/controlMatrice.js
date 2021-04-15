ebb54119f2377b0729793a13463f900dc28aeca9
kevinC <kevin.cariou@transdev.fr>
2021-04-15 17:38:04 +0200

    Création et controle de la matrice achevée

--- js/controlMatrice.js
+++ js/controlMatrice.js
@@ -8,17 +8,17 @@ function controlMain(){
 		let nbCol = parseInt(entree.id.split('_')[1])
 
 		// SI L'INPUT N'EST PAS VIDE, N'EST PAS PRESENTE DANS LE TABLEAU DES IDENTIFIANTS, ET EST UN NOMBRE
-		if ((inputEntree.value !== '') && (tabIdInputEntree.indexOf(idInputEntree) === -1) && !isNaN(inputEntree.value)) {
+		if ((entree.value !== '') && (tabId.indexOf(id_input) === -1) && !isNaN(entree.value)) {
 			comptEntree +=1
-			tabIdInputEntree.push(idInputEntree)
+			tabId.push(id_input)
 		}
 
 		// SI J'ENLEVE UN ELEMENT : L'INPUT NE DOIT PAS ETRE UN NOMBRE, ET DOIT ETRE DEJA PRESENT DANS tabId. CE PARAMETRE PERMET D'EVITER DE CUMULER LES DECREMENTATION DE comptEntree
-		else if (isNaN(inputEntree.value) && (tabIdInputEntree.indexOf(idInputEntree) !== -1)) {
+		else if (isNaN(entree.value) && (tabId.indexOf(id_input) !== -1)) {
 			// JE RETIRE 1 A comptEntree
 			comptEntree -=1
 			// JE RETIRE L'IDENTFIANT DE L'INPUT CIBLE DU TABLEAU AVEC UNE FONCTION QUE J'AI ECRITE EN HAUT
-			tabIdInputEntree.remove(idInputEntree);
+			tabId.remove(id_input);
 		}
 
 		// JE PARCOURS CHAQUE ELEMENT DE elementReadOnly. S'IL Y A UNE CORRESPONDANCE ENTRE L'IDENTIFIANT DE L'ENTREE ET DU TABLEAU, JE MARQUE LA MEME VALEUR
@@ -29,24 +29,8 @@ function controlMain(){
 			}
 		}
 
-		// SI TOUS LES INPUT EDITABLES NE SONT ENTRES....
-		if (comptEntree < ((nbrClass.value * nbrClass.value - nbrClass.value) / 2)) {
-			conteneurAllWithoutCritere[2].style.visibility = 'hidden'
-			conteneurAllWithoutCritere[3].style.visibility = 'hidden'
-			document.getElementById("alerteChamps").style.display = 'block'
-			document.getElementById("alerteCr").style.display = 'none'
-		}
-
-		// SI TOUS LES INPUT EDITABLES SONT ENTRES...
-		if (comptEntree === ((nbrClass.value * nbrClass.value - nbrClass.value) / 2)) {
-			getColumnValues(inputTabMulti)
-			document.getElementById("alerteChamps").style.display = 'none'
-			document.getElementById("alerteCr").style.display = 'block'
-			conteneurAllWithoutCritere[2].style.visibility = 'visible'
-			conteneurAllWithoutCritere[3].style.visibility = 'visible'
-			// document.getElementById("conteneurInfos").style.visibility = 'visible'
-			// document.getElementsByClassName("titre")[2].style.visibility = 'visible'
-		}
+		// SI TOUS LES INPUT EDITABLES ONT ETE ENTRES
+		(comptEntree === ((nbrClass.value * nbrClass.value - nbrClass.value) / 2)) ? allInputFill() : someInputNotFill()
 	}))
 }
 
@@ -66,7 +50,21 @@ function controlMain(){
 // 	}
 // }
 
-// CETTE FONCTION S'ENCLENCHE A CHAQUE FOIS QUE TOUS LES INPUTS SONT REMPLIES. 
+function someInputNotFill(){
+	conteneurAllWithoutCritere[2].style.visibility = 'hidden'
+	conteneurAllWithoutCritere[3].style.visibility = 'hidden'
+	document.getElementById("alerteChamps").style.display = 'block'
+	document.getElementById("alerteCr").style.display = 'none'
+}
+
+function allInputFill(){
+	getColumnValues(inputTabMulti)
+	document.getElementById("alerteChamps").style.display = 'none'
+	document.getElementById("alerteCr").style.display = 'block'
+	conteneurAllWithoutCritere[2].style.visibility = 'visible'
+	conteneurAllWithoutCritere[3].style.visibility = 'visible'
+}
+
 // OBJECTIF 1 : CREE 2 TABLEAUX QUI INDIQUENT QUELLES VALEURS POUR QUELLES COLONNES.
 // OBJECTIF 2 : CREER UN TABLEAU A DOUBLE DIMENSIONS QUI PUISSE ME PERMETTRE DE FACILEMENT FAIRE DES CALCULS SUR LES VALEURS DE CHAQUE COLONNE
 function getColumnValues(inputTabMulti){
